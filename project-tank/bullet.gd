07bcfbc19831066dedd6d2ebcbfdfe11a8d28e8a
Nikhil <nikhil0103@outlook.com>
2025-02-26 21:54:53 -0500

    Fixed bullet collisions
    they work now

--- project-tank/bullet.gd
+++ project-tank/bullet.gd
@@ -2,22 +2,27 @@ extends CharacterBody2D
 
 var bullet
 var num_collisions
+var collidable
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.player_hit.connect(_on_player_hit)
 	num_collisions = 0
+	collidable = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var collision = move_and_collide(velocity * delta)
-	if collision and collision.get_collider().is_class("TileMapLayer"):
+	if collision and collidable:
 		num_collisions += 1
-		if num_collisions == 2:
+		if num_collisions == 2 or collision.get_collider() == bullet:
 			queue_free()
 		var angle_diff = velocity.angle_to(collision.get_normal())
 		velocity = velocity.bounce(collision.get_normal())
 		rotation += 2 * angle_diff
+		collidable = false
+	elif not collision:
+		collidable = true
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
