638397b16f83b274fede92052df8bac6a9a957b5
Nikhil <nikhil0103@outlook.com>
2025-02-28 15:46:48 -0500

    Pre-instantiated enemy
    Instantiated enemy within the main scene rather than within the code for concision and ease of editing

--- project-tank/main.gd
+++ project-tank/main.gd
@@ -1,14 +1,9 @@
 extends Node2D
 
-var enemy = preload("res://enemy.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Global.player_hit.connect(_on_player_hit)
 	Global.enemy_killed.connect(_on_enemy_killed)
-	enemy = enemy.instantiate()
-	enemy.position = Vector2(1000, 288)
-	add_child(enemy)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
