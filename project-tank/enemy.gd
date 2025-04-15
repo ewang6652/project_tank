312b95d83c126c4c1bdc30ec276286e130b4e775
Nikhil <nikhil0103@outlook.com>
2025-04-15 19:19:00 -0400

    Fixed enemy aiming bug
    Enemies were not aiming correctly so I added a bunch of debug stuff to fix it and also the bullets are circles now because circles are easier than rectangles and also I was just doing math wrong but I don't feel like changing the bullets back to rectangles and we might not need to anyways so I don't really care about doing that.
    
    How long can these commit messages be?

--- project-tank/enemy.gd
+++ project-tank/enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var screen_size
 var bullet_scene = preload("res://bullet.tscn")
+#var raycast_line = Line2D.new()
 @export_range(0, 360, 1, "radians_as_degrees", "suffix:°/s") var turret_angular_speed = PI/2  # per second
 ## How many times bullets shot by this enemy can bounce off walls
 @export_range(0, 3) var max_ricochets = 1
@@ -12,26 +13,46 @@ var bullet_scene = preload("res://bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#add_child(raycast_line)
 	screen_size = get_viewport().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+# Called every physics frame.
+func _physics_process(delta):
+	var shooting = false
+	for ricochets in range(max_ricochets+1):  # can we hit the player with 0 ricochets? 1? ...
+		for theta in range(0, 360, 5):
+			if raycast(deg_to_rad(theta), ricochets):
+				rotate_turret(deg_to_rad(theta), delta)
+				shooting = true
+				break
+		if shooting:
+			break
+	if not shooting:  # the player cannot be hit, so move
+		var curr_pos = global_position
+		
+
 func raycast(angle, ricochets=1):  # returns true if player is found
 	var space_state = get_world_2d().direct_space_state
 	var from = global_position
 	var direction = Vector2.from_angle(angle)
 	var to = from + direction * (screen_size.x ** 2 + screen_size.y ** 2) ** 0.5
+	#raycast_line.clear_points()
+	#raycast_line.add_point(from - global_position)
 	for ricochet in range(ricochets+1):  # one for the initial shot, then the ricochets
 		var query = PhysicsRayQueryParameters2D.create(from, to, raycast_mask, [self])
 		var result = space_state.intersect_ray(query)
 		if not result:
 			return false
+		#raycast_line.add_point(result.position - global_position - Vector2.from_angle(angle) * 5)
 		if result.collider.name == "Player":
 			return true
-		from = result.position
-		to = to.bounce(result.normal)
+		from = result.position - Vector2.from_angle(angle) * 5
+		direction = direction.bounce(result.normal)
+		to = from + direction * (screen_size.x ** 2 + screen_size.y ** 2) ** 0.5
 	return false
 
 func rotate_turret(target_angle, delta):
@@ -53,18 +74,6 @@ func shoot():
 		bullet.velocity = Vector2(0.0, bullet_speed).rotated(bullet.rotation)
 		add_sibling(bullet)
 		$Timer.start()
-	
-
-func _physics_process(delta):
-	var done = false
-	for ricochets in range(max_ricochets+1):  # can we hit the player with 0 ricochets? 1? ...
-		for theta in range(360):
-			if raycast(deg_to_rad(theta), ricochets):
-				rotate_turret(deg_to_rad(theta), delta)
-				done = true
-				break
-		if done:
-			break
 
 func die():
 	Global.enemy_killed.emit()
